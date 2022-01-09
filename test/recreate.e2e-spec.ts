464388840cf426694113340a9c2cc4ec5073cd9e
Brendan-Stubbs <48714800+Brendan-Stubbs@users.noreply.github.com>
2022-01-09 11:34:29 +0200

    reproduce trx bug

---
+++ test/recreate.e2e-spec.ts
@@ -0,0 +1,25 @@
+import { Test, TestingModule } from '@nestjs/testing';
+import { INestApplication } from '@nestjs/common';
+import * as request from 'supertest';
+import { AppModule } from '../src/app.module';
+
+describe('Recreate Bug', () => {
+  let app: INestApplication;
+
+  beforeAll(async () => {
+    const moduleFixture: TestingModule = await Test.createTestingModule({
+      imports: [AppModule],
+    }).compile();
+
+    app = moduleFixture.createNestApplication();
+    await app.init();
+  });
+
+  afterAll(async () => {
+    await app.close();
+  });
+
+  it('It should successfully use an interactive transaction', async () => {
+    await request(app.getHttpServer()).post('/organizations').expect(201);
+  });
+});
